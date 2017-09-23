84faec605998d33b07eafca338af32c216ef2246
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-09-23 16:36:15 +0100

    Add DbContract and DbHelper

---
+++ app/src/main/java/com/example/user/foodtracker/DbContract.java
@@ -0,0 +1,28 @@
+package com.example.user.foodtracker;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by user on 22/09/2017.
+ */
+
+public class DbContract {
+    private DbContract(){}
+
+    public static class FeedAvailableFoods implements BaseColumns {
+        public static final String TABLE_NAME = "available_foods";
+        public static final String COL1 = "name";
+        public static final String COL2 = "kcal";
+        public static final String COL3 = "carbohydrates";
+        public static final String COL4 = "fat";
+        public static final String COL5 = "protein";
+    }
+
+
+    public static class FeedFoodHistory implements BaseColumns {
+        public static final String TABLE_NAME = "food_history";
+        public static final String COL1 = "date";
+        public static final String COL2 = "food_id";
+        public static final String COL3 = "quantity";
+    }
+}
---
+++ app/src/main/java/com/example/user/foodtracker/DbHelper.java
@@ -0,0 +1,54 @@
+package com.example.user.foodtracker;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by user on 22/09/2017.
+ */
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "food.db";
+    public static final String Table1 = "available_foods";
+    public static final String Table2 = "food_history";
+
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + DbContract.FeedAvailableFoods.TABLE_NAME + " (" +
+                    DbContract.FeedAvailableFoods._ID + " INTEGER PRIMARY KEY," +
+                    DbContract.FeedAvailableFoods.COL1 + " TEXT," +
+                    DbContract.FeedAvailableFoods.COL2 + " FLOAT," +
+                    DbContract.FeedAvailableFoods.COL3 + " FLOAT," +
+                    DbContract.FeedAvailableFoods.COL4 + " FLOAT," +
+                    DbContract.FeedAvailableFoods.COL5 + " FLOAT);" +
+            "CREATE TABLE " + DbContract.FeedFoodHistory.TABLE_NAME + " (" +
+                    DbContract.FeedFoodHistory._ID + " INTEGER PRIMARY KEY," +
+                    DbContract.FeedFoodHistory.COL1 + " DATE," +
+                    DbContract.FeedFoodHistory.COL2 + " INTEGER REFERENCES available_foods(_id)," +
+                    DbContract.FeedFoodHistory.COL3 + " FLOAT)";
+
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + DbContract.FeedAvailableFoods.TABLE_NAME +
+            "DROP TABLE IF EXISTS " + DbContract.FeedFoodHistory.TABLE_NAME;
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+}
