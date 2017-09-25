6df035f567e4993081bd51100c7454d9137365e0
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-09-25 13:34:57 +0100

    Fix Toolbar bug

--- app/src/main/java/com/example/user/foodtracker/WelcomeActivity.java
+++ app/src/main/java/com/example/user/foodtracker/WelcomeActivity.java
@@ -4,17 +4,22 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+//import android.widget.Toolbar;
 
 public class WelcomeActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
