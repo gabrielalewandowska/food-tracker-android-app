d31f817d455fcb0e6aeed4f254f9ef5203875c39
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-09-25 15:30:16 +0100

    MenuItems in NavView send to different activities

--- app/src/main/java/com/example/user/foodtracker/WelcomeActivity.java
+++ app/src/main/java/com/example/user/foodtracker/WelcomeActivity.java
@@ -1,17 +1,22 @@
 package com.example.user.foodtracker;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 //import android.widget.Toolbar;
 
 public class WelcomeActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private Toolbar mToolbar;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +30,45 @@ public class WelcomeActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem){
+                switch (menuItem.getItemId()){
+                    case(R.id.nav_diary):
+                        Intent accountActivity = new Intent(getApplicationContext(), AddNewFoodActivity.class);
+                        startActivity(accountActivity);
+
+
+                    case(R.id.nav_add_food):
+                        Intent accountActivity2 = new Intent(getApplicationContext(), AddNewFoodActivity.class);
+                        startActivity(accountActivity2);
+
+
+                    case(R.id.nav_history):
+                        Intent accountActivity3 = new Intent(getApplicationContext(), ShowHistoryActivity.class);
+                        startActivity(accountActivity3);
+                }
+                  return true;
+
+                }
+
+
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
 
         if(mToggle.onOptionsItemSelected(item)){
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
