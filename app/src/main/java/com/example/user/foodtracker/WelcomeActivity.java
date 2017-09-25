34f131182a8e3c37cd95ab18462b7e384bc7b5bf
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-09-25 11:28:09 +0100

    Add drawer navigation with toggle and icons

--- app/src/main/java/com/example/user/foodtracker/WelcomeActivity.java
+++ app/src/main/java/com/example/user/foodtracker/WelcomeActivity.java
@@ -1,38 +1,33 @@
 package com.example.user.foodtracker;
 
-import android.database.Cursor;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MenuItem;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        TextView tv = (TextView) findViewById(R.id.textView);
-        DbHelper db = new DbHelper(this);
-        db.deleteAllFoodItems();
-        db.addNewFood("oats", 389, 66.3, 6.9, 16.9);
-        db.addNewFood("bananas", 89, 22.8, 0.3, 1.1);
-        db.addNewFood("sweet potatoes", 86, 20.12, 0.05, 1.6);
-        tv.setText("");
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
-        FoodItem foodItem = db.returnAllAvailableFoods();
-        tv.setText(foodItem.getName() + "kcal: " + foodItem.getKcal());
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-//        Cursor k = db.returnAllAvailableFoods();
-//        while(k.moveToNext()){
-//            Integer id = k.getInt(0);
-//            String name = k.getString(1);
-//            Integer kcal = k.getInt(2);
-//            Double carbs = k.getDouble(3);
-//            Double fat = k.getDouble(4);
-//            Double protein = k.getDouble(5);
-//            tv.setText(tv.getText() + "\n" + id + " " + name + " " + kcal + " " + carbs +
-//            " " + fat + " " + protein);
-//        }
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
