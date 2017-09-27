68497e1a4910139b2017a1a33c0ebd08264057cf
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-09-27 01:27:19 +0100

    Add diaryFragment, FragmentManager and FragmentTransaction to MainActivity

--- app/src/main/java/com/example/user/foodtracker/MainActivity.java
+++ app/src/main/java/com/example/user/foodtracker/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.user.foodtracker;
 
 import android.app.DialogFragment;
-import android.icu.text.SimpleDateFormat;
-import android.icu.util.Calendar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,15 +14,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     String selectedDate;
+    FragmentManager fragmentManager;
+    DiaryFragment diaryFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +44,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        fragmentManager = getFragmentManager();
+        diaryFragment = (DiaryFragment) getSupportFragmentManager().findFragmentById(R.id.diaryFragment);
+        FragmentTransaction fragmentTransactionGeneral = fragmentManager.beginTransaction();
 
         DiaryFragment fragment = new DiaryFragment();
         android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -102,7 +103,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.diaryFragment) {
-            setTitle("Diary Fragment");
+            setTitle("Diary");
             DiaryFragment fragment = new DiaryFragment();
             android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
             fragmentTransaction.replace(R.id.frame, fragment, "Food Diary");
